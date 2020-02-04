88d2829e6c3363aa947af931f30b21a0050d67eb
singwa@imooc.com <singwa@imooc.com>
2020-02-04 23:19:30 +0800

    修改第九章页面BUG

--- admin_tpl/9_chapter/js/uploads.js
+++ admin_tpl/9_chapter/js/uploads.js
@@ -4,58 +4,64 @@ layui.use(['upload'], function () {
 
     var uploadInst = upload.render({
         elem: '#btn_main'
-        ,url: '/upload/'
-        ,before: function(obj){
-            //预读本地文件示例，不支持ie8 注释掉
-            obj.preview(function(index, file, result){
-                // $('#main_img').attr('src', result); //图片链接（base64）
-                $('.main_img').html('<img  src="'+result+'"  class="layui-upload-img upload-img" id="main_img">'); //图片链接（base64）
-            });
-        }
+        ,url: '/admin/image/upload'
         ,done: function(res){
             //如果上传失败
-            if(res.code > 0){
+            if(res.status == 0){
                 return layer.msg('上传失败');
             }
-            $('.main_img').html('<img  src="'+res+'"  class="layui-upload-img upload-img" id="main_img">');
+            $('.big_image').html('<img  src="'+res.result.image+'"  class="layui-upload-img upload-img" id="main_img">');
         }
     });
 
 
         upload.render({
             elem: '#btn_banner'
-            ,url: '/upload/'
+            ,url: '/admin/image/upload'
             ,multiple: true
-            ,before: function(obj){
-                //预读本地文件示例，不支持ie8 注释掉
-                obj.preview(function(index, file, result){
-                    $('#banner_img').append('<div class="img-wrap"><img src="'+ result +'" alt="'+ file.name +'" class="layui-upload-img upload-img"><i class="layui-icon layui-icon-close btn-del"></i></div>')
-                });
-            }
+            // ,before: function(obj){
+            //     //预读本地文件示例，不支持ie8 注释掉
+            //     obj.preview(function(index, file, result){
+            //         $('#banner_img').append('<div class="img-wrap"><img src="'+ result +'" alt="'+ file.name +'" class="layui-upload-img upload-img"><i class="layui-icon layui-icon-close btn-del"></i></div>')
+            //     });
+            // }
             ,done: function(res){
 
-                obj.preview(function(index, file, result){
-                    $('#banner_img').append('<div class="img-wrap"><img src="'+ result +'"  class="layui-upload-img upload-img"><i class="layui-icon layui-icon-close btn-del"></i></div>')
-                });
+                //如果上传失败
+                if(res.status == 0){
+                    return layer.msg('上传失败');
+                }
+                $('#banner_img').append('<div class="img-wrap"><img src="'+ res.result.image +'"  class="layui-upload-img upload-img"><i class="layui-icon layui-icon-close btn-del"></i></div>')
+
+
+                // obj.preview(function(index, file, result){
+                //     $('#banner_img').append('<div class="img-wrap"><img src="'+ result +'"  class="layui-upload-img upload-img"><i class="layui-icon layui-icon-close btn-del"></i></div>')
+                // });
                 //上传完毕
             }
         });
 
         upload.render({
             elem: '#btn_show'
-            ,url: '/upload/'
+            ,url: '/admin/image/upload'
             ,multiple: true
-            ,before: function(obj){
-                //预读本地文件示例，注释掉
-                obj.preview(function(index, file, result){
-                    $('#show_img').append('<div class="img-wrap"><img src="'+ result +'"  class="layui-upload-img upload-img"><i class="layui-icon layui-icon-close btn-del"></i></div>')
-                });
-            }
+            // ,before: function(obj){
+            //     //预读本地文件示例，注释掉
+            //     obj.preview(function(index, file, result){
+            //         $('#show_img').append('<div class="img-wrap"><img src="'+ result +'"  class="layui-upload-img upload-img"><i class="layui-icon layui-icon-close btn-del"></i></div>')
+            //     });
+            // }
             ,done: function(res){
-                obj.preview(function(index, file, result){
-                    $('#show_img').append('<div class="img-wrap"><img src="'+ result +'"  class="layui-upload-img upload-img"><i class="layui-icon layui-icon-close btn-del"></i></div>')
-                });
+                //如果上传失败
+                if(res.status == 0){
+                    return layer.msg('上传失败');
+                }
+                // obj.preview(function(index, file, result){
+                //     $('#show_img').append('<div class="img-wrap"><img src="'+ result +'"  class="layui-upload-img upload-img"><i class="layui-icon layui-icon-close btn-del"></i></div>')
+                // });
                 //上传完毕
+                $('#show_img').html('<div class="img-wrap"><img src="'+ res.result.image +'"  class="layui-upload-img upload-img"><i class="layui-icon layui-icon-close btn-del"></i></div>')
+
             }
         });
 
